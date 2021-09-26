3706bcb6fa0e7dcb3a55c675c69858cfc23299a3
manaskumar2808 <72543699+manaskumar2808@users.noreply.github.com>
2021-09-26 11:09:35 +0530

    Delete smurwafer directory

--- smurwafer/src/validators/password/password-validator.ts
+++
@@ -1,9 +0,0 @@
-import { body } from 'express-validator';
-
-const validator = [
-    body('newPassword')
-        .isLength({ min: 7 })
-        .withMessage('Password length must be atleast 8'),
-];
-
-export { validator as PasswordValidator };
